1eef253a89c7b04769f67c087b4bebe917c09810
TwinFlame University <61005029+Twin-Flame@users.noreply.github.com>
2024-10-10 09:08:12 -0400

    Create restore.sh

---
+++ restore/KVM/restore.sh
@@ -0,0 +1,6 @@
+name=win11
+
+sudo rsync -av --progress --append-verify ${name}.compressed.qcow2 /var/lib/libvirt/images/${name}.qcow2
+sudo rsync -av --progress --append-verify ${name}_VARS.fd /var/lib/libvirt/qemu/nvram/
+cd xml
+define gpu.xml

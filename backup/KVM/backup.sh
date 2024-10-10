ff4241597c592ffd37f9ccf6da0bedf6ebc5dca5
TwinFlame University <61005029+Twin-Flame@users.noreply.github.com>
2024-10-10 11:36:20 -0400

    Update backup.sh

--- backup/KVM/backup.sh
+++ backup/KVM/backup.sh
@@ -7,4 +7,4 @@ sudo rsync -av --progress --append-verify /var/lib/libvirt/qemu/nvram/${name}_VA
 
 sudo qemu-img convert -p -O qcow2 -c ${name}.qcow2 ${name}.compressed.qcow2
 
-virsh dump ${name} > xml/original.xml
+virsh dumpxml ${name} > xml/original.xml

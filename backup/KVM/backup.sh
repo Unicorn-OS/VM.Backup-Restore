07071fcd1ecd2cabe93d8e4e3fbccf14cab06f77
TwinFlame University <61005029+Twin-Flame@users.noreply.github.com>
2024-10-10 11:27:59 -0400

    Create backup.sh

---
+++ backup/KVM/backup.sh
@@ -0,0 +1,10 @@
+name=win11
+
+mkdir -p xml nvram
+
+sudo mv /var/lib/libvirt/images/${name}.qcow2 . 
+sudo rsync -av --progress --append-verify /var/lib/libvirt/qemu/nvram/${name}_VARS.fd nvram/  
+
+sudo qemu-img convert -p -O qcow2 -c ${name}.qcow2 ${name}.compressed.qcow2
+
+virsh dump ${name} > xml/original.xml

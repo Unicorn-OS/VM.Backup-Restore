02006a350da51be2e95fae5a047637de4662493e
DarkStar <darkstar@unisocial.net>
2024-10-16 03:39:40 +0300

    0

--- bash/KVM/.template/UEFI/restore.sh
+++ bash/KVM/.template/UEFI/restore.sh
@@ -3,4 +3,4 @@ source var
 sudo rsync -av --progress --append-verify ${name}.compressed.qcow2 /var/lib/libvirt/images/${name}.qcow2
 sudo rsync -av --progress --append-verify ${name}_VARS.fd /var/lib/libvirt/qemu/nvram/
 cd xml
-virsh define gpu.xml
+virsh define original.xml
